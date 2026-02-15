20af5a2832018fd106d84fe260e8b2129e885934
donna <donnamarijne+gh@gmail.com>
2026-02-15 15:48:21 +0000

    1.11 custom handlers

---
+++ healthhandler.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func healthHandler(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte("OK"))
+}
--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 func main() {
 	port := "8080"
 
-	handler := http.NewServeMux()
+	mux := http.NewServeMux()
 
-	handler.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", healthHandler)
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	server := http.Server{
 		Addr:    ":" + port,
-		Handler: handler,
+		Handler: mux,
 	}
 
 	log.Printf("Starting Chirpy on port %s...", port)
